09d53c2b66b2e807c30be41aaf56f021b279dddb
mbshealthtech <turbo.paypal.systems@gmail.com>
2017-06-17 01:38:29 +0800

    Update lol.php

--- lol.php
+++ lol.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_GET["id"];
-$file = "hide" . $id . ".mp4";
+$file = "hide98" . $id . ".mp4";
 
 if (file_exists($file)) {
 readfile('http://www.hideurlscript.com/scripttest/lol.php');
